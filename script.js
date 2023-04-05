02838e0c8fe66276f22807aaca57dd819354f8aa
dsouza <dhesouzanorbert36@gmail.com>
2023-04-05 23:48:11 +0300

    Added an event listener to the button

---
+++ script.js
@@ -0,0 +1,32 @@
+// Length (meters/feet)
+let inputMeters = document.getElementById('input-meters');
+console.log(inputMeters.textContent);
+let inputFeets = document.getElementById('input-feets');
+console.log(inputFeets.textContent);
+
+let resultsFeets = document.getElementById('results-feets');
+console.log(resultsFeets.textContent);
+let resultsMeters = document.getElementById('results-meters');
+console.log(resultsMeters.textContent);
+
+// Volume (Liters/Gallons)
+let inputLiters = document.getElementById('input-liters');
+console.log(inputLiters.textContent);
+let inputGallons = document.getElementById('input-gallons');
+console.log(inputGallons.textContent);
+
+let resultsGallons = document.getElementById('results-gallons');
+console.log(resultsGallons.textContent);
+let resultsLiters = document.getElementById('results-liters');
+console.log(resultsLiters.textContent);
+
+// Mass (Kilograms/Pounds)
+let inputKilos = document.getElementById('input-kilos');
+console.log(inputKilos.textContent);
+let inputPounds = document.getElementById('input-pounds');
+console.log(inputPounds.textContent);
+
+let resultsPounds = document.getElementById('results-pounds');
+console.log(resultsPounds.textContent);
+let resultsKilos = document.getElementById('results-kilos');
+console.log(resultsKilos.textContent);
